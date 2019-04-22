998249dc761dbe5d94b7253e7f41a0f8e2610c3c
github username <github email address>
2019-04-22 14:48:21 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+let feet = 264
+function distanceFromHqInBlocks(value) {
+  if (value < 42) {
+    return 42 - value
+  } else {
+    return value - 42
+  }
+}
+
+function distanceFromHqInFeet (someValue) {
+  return distanceFromHqInBlocks(someValue) * feet
+}
+
+function distanceTravelledInFeet (first, second) {
+  if (first > second) {
+    return (first - second) * feet
+  } else {
+    return (second - first) * feet
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start,destination)
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
